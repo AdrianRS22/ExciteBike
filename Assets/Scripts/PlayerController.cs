df9165f5420b00d690ff08a2997500c36f6be14c
Adrian <adrianrojasanchez@gmail.com>
2021-03-19 17:23:05 -0600

    Usando el input manager

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public Transform target;
+
+    [SerializeField]
+    public Transform target2;
+
+    [SerializeField]
+    public float speed;
+
+    [SerializeField]
+    public Rigidbody2D body;
+
+    Vector3 currentPosition;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        currentPosition = transform.position;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            currentPosition = target.position;
+            currentPosition.z = 1F;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentPosition, speed * Time.deltaTime);
+    }
+}
