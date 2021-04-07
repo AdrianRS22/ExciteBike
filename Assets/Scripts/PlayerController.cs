2e8ca8a50a435e84e8958255059fec3140ab3477
Adrian <adrianrojasanchez@gmail.com>
2021-04-07 17:07:33 -0600

    Eliminando constantes innecesarias

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     public float speed = 5.0f;
 
-    private const string AXIS_H = "Horizontal", AXIS_V = "Vertical";
-
     private Animator animator;
 
     private bool isRiding = false;
@@ -24,7 +22,7 @@ public class PlayerController : MonoBehaviour
         {
             isRiding = true;
             var translation =
-                new Vector3(-Input.GetAxisRaw(AXIS_H) * speed * Time.deltaTime, 0, 0);
+                new Vector3(-Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, 0);
             transform.Translate(translation);
         }
 
@@ -33,18 +31,17 @@ public class PlayerController : MonoBehaviour
             isRiding = false;
         }
 
-        if (Mathf.Abs(Input.GetAxisRaw(AXIS_V)) > 0.2f)
+        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.2f)
         {
             var translation =
-                new Vector3(0, Input.GetAxisRaw(AXIS_V) * speed * Time.deltaTime, 0);
+                new Vector3(0, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0);
             transform.Translate(translation);
         }
     }
 
     private void LateUpdate()
     {
-        animator.SetFloat("Horizontal", Input.GetAxisRaw(AXIS_H));
-        animator.SetFloat("Vertical", Input.GetAxisRaw(AXIS_V));
+        animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
         animator.SetBool("isRiding", isRiding);
     }
 }
