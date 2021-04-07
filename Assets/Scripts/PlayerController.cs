76d4a69bff9056851bce63fb89507fa84a08df8a
Adrian <adrianrojasanchez@gmail.com>
2021-04-07 16:28:59 -0600

    Animacion y moviendo el personaje

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,33 +5,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    public float speed;
+    public float speed = 5.0f;
 
-    [SerializeField]
-    public Rigidbody2D body;
+    private const string AXIS_H = "Horizontal", AXIS_V = "Vertical";
 
-    Vector3 currentPosition;
+    private Animator animator;
 
-    void Awake()
-    {
-        body = GetComponent<Rigidbody2D>();
-    }
+    private bool isRiding = false;
 
     void Start()
     {
-        currentPosition = transform.position;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.A))
         {
-            currentPosition.y = -1.9F;
+            isRiding = true;
+            var translation =
+                new Vector3(-Input.GetAxisRaw(AXIS_H) * speed * Time.deltaTime, 0, 0);
+            transform.Translate(translation);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            isRiding = false;
+        }
+
+        if (Mathf.Abs(Input.GetAxisRaw(AXIS_V)) > 0.2f)
+        {
+            var translation =
+                new Vector3(0, Input.GetAxisRaw(AXIS_V) * speed * Time.deltaTime, 0);
+            transform.Translate(translation);
         }
     }
 
-    void FixedUpdate()
+    private void LateUpdate()
     {
-        body.position = Vector3.MoveTowards(body.position, currentPosition, speed * Time.deltaTime);
+        animator.SetFloat("Horizontal", Input.GetAxisRaw(AXIS_H));
+        animator.SetFloat("Vertical", Input.GetAxisRaw(AXIS_V));
+        animator.SetBool("isRiding", isRiding);
     }
 }
