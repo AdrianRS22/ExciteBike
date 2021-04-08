1a325fa2b8b41d4449595393466b9c3ccc98d7ab
Adrian <adrianrojasanchez@gmail.com>
2021-04-07 19:33:50 -0600

    Cine Machine incluido

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var position = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = position;
+    }
+}
