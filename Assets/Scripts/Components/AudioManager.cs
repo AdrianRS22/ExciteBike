43a6ce4cd35c8647ceb649f5ea08eda6b68f3cbd
Adrian <adrianrojasanchez@gmail.com>
2021-04-10 12:12:49 -0600

    AudioManager agregado

---
+++ Assets/Scripts/Components/AudioManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Start()
+    {
+        Play("Theme");
+    }
+
+    void Awake()
+    {
+        foreach(var sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.sonido;
+            sound.source.volume = sound.volumen;
+        }
+    }
+
+    public void Play(string nombre)
+    {
+        var sound = Array.Find(sounds, x => x.nombre.Equals(nombre));
+
+        if(sound == null)
+        {
+            Debug.LogFormat("El audio {0} no fue encontrado.", nombre);
+            return;
+        }
+
+        sound.source.Play();
+    }
+}
---
+++ Assets/Scripts/Components/Sound.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Sound
+{
+    [SerializeField]
+    public string nombre;
+
+    [SerializeField]
+    public AudioClip sonido;
+
+    [HideInInspector]
+    public AudioSource source;
+
+    [SerializeField]
+    [Range(0F, 1F)]
+    public float volumen;
+}
