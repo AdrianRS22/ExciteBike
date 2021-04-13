7ce06c5b38c8e571674ef5eae5ca1f31bf4a784e
Adrian <adrianrojasanchez@gmail.com>
2021-04-13 16:31:59 -0600

    mostrando el score en el gameover

--- Assets/Scripts/SceneReturn.cs
+++ Assets/Scripts/SceneReturn.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
 public class SceneReturn : MonoBehaviour
 {
+    public Text scoreText;
     private SceneLoader sceneLoader;
+
+    void Start()
+    {
+        var scoreList = SaveLoadManager.LoadData<List<Score>>("Data", "score");
+        var recentScore = scoreList.OrderByDescending(o => o.dateCreated).First();
+        scoreText.text = recentScore.text;
+    }
 
     void Awake()
     {
