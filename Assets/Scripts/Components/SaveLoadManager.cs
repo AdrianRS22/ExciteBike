571dfd5e02f5304d8e4edfbc1027a39451ad4fbf
Adrian <adrianrojasanchez@gmail.com>
2021-04-13 13:57:59 -0600

    Creando script del save load para guardar o cargar datos

---
+++ Assets/Scripts/Components/SaveLoadManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.IO;
+
+public static class SaveLoadManager
+{
+    public static void SaveData<T>(T data, string path, string fileName)
+    {
+        string fullPath = $"{Application.persistentDataPath}/{path}/";
+        bool folderExist = Directory.Exists(fullPath);
+
+        if (!folderExist)
+        {
+            Directory.CreateDirectory(fullPath);
+        }
+        string jsonData = JsonUtility.ToJson(data);
+        File.WriteAllText(fullPath + fileName + ".json", jsonData);
+        Debug.LogFormat("El dato ha sido guardado correctamente en {0}", fullPath);
+    }
+
+    public static T LoadData<T>(string path, string fileName)
+    {
+        string fullPath = $"{Application.persistentDataPath}/{path}/{fileName}.json";
+        if (File.Exists(fullPath))
+        {
+            string textJson = File.ReadAllText(fullPath);
+            var fileObject = JsonUtility.FromJson<T>(textJson);
+            return fileObject;
+        }
+        else
+        {
+            return default;
+        }
+    }
+}
